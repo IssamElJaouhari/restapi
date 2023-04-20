470c96c55767b961b7dc49aeb6b16971f27a7c1b
Issam  El  Jaouhari <46448896+IssamElJaouhari@users.noreply.github.com>
2023-04-20 14:11:37 +0000

    Add files via upload

---
+++ server.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const User = require('./models/User');
+
+
+mongoose.connect('mongodb+srv://issamefar58:issamassi123@cluster0.jnz5nwy.mongodb.net/contact', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("Database connected successfully");
+    })
+    .catch((err) => {
+        console.log("Database connection failed", err);
+    });
+
+app.use(express.json());
+
+app.get('/users', async (req, res) => {
+    try {
+        const users = await User.find();
+        res.json(users);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+app.post('/users', async (req, res) => {
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        age: req.body.age
+    });
+
+    try {
+        const newUser = await user.save();
+        res.status(201).json(newUser);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
+
+app.put('/users/:id', getUser, async (req, res) => {
+    if (req.body.name != null) {
+        res.user.name = req.body.name;
+    }
+
+    if (req.body.email != null) {
+        res.user.email = req.body.email;
+    }
+
+    if (req.body.age != null) {
+        res.user.age = req.body.age;
+    }
+
+    try {
+        const updatedUser = await res.user.save();
+        res.json(updatedUser);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
+
+app.delete('/users/:id', getUser, async (req, res) => {
+    try {
+        await res.user.remove();
+        res.json({ message: 'User deleted' });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+async function getUser(req, res, next) {
+    try {
+        const user = await User.findById(req.params.id);
+        if (user == null) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        res.user = user;
+        next();
+    } catch (err) {
+        return res.status(500).json({ message: err.message });
+    }
+}
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
